fa1260d754a9d8972cdb527a02b650c343281d8a
Joel Garcia Jr <joel.garciajr84@gmail.com>
2014-10-21 16:26:27 -0200

    Condicionais e loops

---
+++ src/CondicionaiseLoops.java
@@ -0,0 +1,34 @@
+
+public class CondicionaiseLoops {
+	
+	public static void main(String[] args){
+		
+// if else		
+		int idade = 25;
+		
+		if (idade >= 18){
+			
+			System.out.println("Entrada Permitida");
+			
+		}else{
+			
+			System.out.println("Entrada Proibida");
+		}
+		
+// Expressoes Booleanas
+		
+	boolean VotanoPT = true;
+	
+	if (VotanoPT == true && idade >= 16){
+		
+		System.out.println("Parabens vc escolheu corretamente");
+	}else{
+		
+		System.out.println("Nao autorizado ou votou errado");
+	}
+	
+	
+		
+	}
+
+}
